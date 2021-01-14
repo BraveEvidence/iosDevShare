7af882a172c2b7f3bc3205f327e9fdfe4e59e8d7
pritishsawant <sawantpritish@gmail.com>
2021-01-14 13:43:56 +0530

    Initial Commit

---
+++ iosdevshare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosdevshare
+//
+//  Created by Pritish Sawant on 14/01/21.
+//  Copyright © 2021 Pritish Sawant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
